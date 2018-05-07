23ea2808580da206b4b69bb5918f7ce9e08efae7
imkrish07 <singhkrishk07@gmail.com>
2018-05-07 22:26:20 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function opensidemenu(){
+    document.getElementById('side-menu').style.width='250px';
+    document.getElementById('main').style.marginLeft='250px';
+    
+}
+function closesidemenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+
+}
